7ba0ea0b22d0c74831391bb60e2a99721c324107
Ilya Yampolsky <cfnils@gmail.com>
2016-05-27 17:05:30 -0400

    authenticated password

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,8 +1,19 @@
-#get login
 get '/login' do
   erb :'sessions/login'
 end
 
+post '/sessions' do
+  @user = User.find_by(email: params[:user][:email])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:id] = @user.id
+    redirect '/decks'
+  else
+    @error = User.new(params[:user]).errors.full_messages
+    erb :'sessions/login'
+  end
+end
 
-#post login
-#get /logout
+get '/logout' do
+  session.clear
+  redirect '/login'
+end
